827957b13ace7350c095d0486b7a2dbba53b340b
Anorak Tha Great <anorakthagreat@gmail.com>
2019-11-28 01:27:44 -0700

    soft fork activation of block version 5

--- src/main.h
+++ src/main.h
@@ -1031,7 +1031,7 @@ class CBlock
 {
 public:
     // header
-    static const int CURRENT_VERSION=4;
+    static const int CURRENT_VERSION=5;
     int nVersion;
     uint256 hashPrevBlock;
     uint256 hashMerkleRoot;
