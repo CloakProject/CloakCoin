efc024bb030f00f93ca3313d884015126641dae1
anorakthagreat <33412340+anorakthagreat@users.noreply.github.com>
2026-01-18 13:30:42 -0700

    Bump block version to 6
    bump block CURRENT_VERSION to 6 to address the stale UTXO stake bug

--- src/main.h
+++ src/main.h
@@ -1031,7 +1031,7 @@ class CBlock
 {
 public:
     // header
-    static const int CURRENT_VERSION=5;
+    static const int CURRENT_VERSION=6;
     int nVersion;
     uint256 hashPrevBlock;
     uint256 hashMerkleRoot;
